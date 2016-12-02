803338752b74c615b50da4fd4f13ba7b6b47e9f4
Hyungmo Gu <guhyungm7@gmail.com>
2016-12-02 15:52:15 -0800

    persist data by integrating postgreSQL into respnsible functions

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -3,9 +3,7 @@
 # 
 
 import time
-
-## Database connection
-DB = []
+import psycopg2
 
 ## Get posts from database.
 def GetAllPosts():
@@ -16,9 +14,16 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    conn = psycopg2.connect(database='forum')
+    cur = conn.cursor()
+    cur.execute('SELECT content,time FROM posts;')
+    rows = cur.fetchall() 
+
+    posts = [{'content': str(row[0]), 'time': str(row[1])} for row in rows]
+    posts.sort(key=lambda row: row['time'], reverse=True) #hey, how does this work?
     return posts
+
+    conn.close()
 
 ## Add a post to the database.
 def AddPost(content):
@@ -27,5 +32,16 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    conn = psycopg2.connect(database='forum')
+    cur = conn.cursor()
+    postDateAndTime = getPostDateAndTime()
+    cur.execute('''INSERT INTO posts (content,time) VALUES (%s,%s);''',(content,postDateAndTime))
+
+    conn.commit()
+    conn.close()
+
+def getPostDateAndTime():
+  postDate = "-".join([ str(i) for i in time.localtime()[:3]])
+  postTime = ":".join([ str(i) for i in time.localtime()[3:6]])
+  postDateAndTime = " ".join([postDate,postTime])
+  return postDateAndTime
