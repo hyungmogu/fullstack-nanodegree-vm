655e36db7914c9cd3c3beb6635659c2369ce8a03
Hyungmo Gu <guhyungm7@gmail.com>
2017-01-31 21:30:43 -0800

    Refactor tournament.sql

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -1,4 +1,4 @@
-;-- Table definitions for the tournament project.
+-- Table definitions for the tournament project.
 --
 -- Put your SQL 'create table' statements in this file; also 'create view'
 -- statements if you choose to use it.
@@ -23,16 +23,17 @@ CREATE TABLE match_result (
 	loser_id INT REFERENCES player(id)
 ); 
 
+-- Count number of wins per player id
 CREATE VIEW wins AS
 SELECT t.id, COUNT(t.winner_id)
 FROM (SELECT p.id AS id, m.winner_id AS winner_id FROM player p LEFT JOIN match_result m ON p.id = m.winner_id) AS t 
 GROUP BY t.id; 
-
+-- Count number of wins per player id
 CREATE VIEW losses AS
 SELECT t.id, COUNT(t.loser_id)
 FROM (SELECT p.id AS id, m.loser_id AS loser_id FROM player p LEFT JOIN match_result m ON p.id = m.loser_id) AS t
 GROUP BY t.id;
-
+-- Combine wins and losses table
 CREATE VIEW player_standings AS
 SELECT t.id, t.name, t.wins, t.wins + losses.count AS matches
 FROM (SELECT player.id, player.name, wins.count AS wins
