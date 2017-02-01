d57daf4f3887386300aabfa838dc00f2945df85f
Hyungmo Gu <guhyungm7@gmail.com>
2017-01-31 20:11:08 -0800

    Update tournament.sql

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,15 +6,36 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+DROP DATABASE IF EXISTS tournament;
 
-CREATE TABLE players (
+CREATE DATABASE tournament;
+
+\c tournament
+
+CREATE TABLE player (
 	id SERIAL PRIMARY KEY,
 	name TEXT NOT NULL
 );
 
-CREATE TABLE matches (
+CREATE TABLE match_result (
 	id SERIAL PRIMARY KEY,
-	wins INT NOT NULL,
-	losses INT NOT NULL,
-	matches INT NOT NULL
+	winner_id INT REFERENCES player(id),
+	loser_id INT REFERENCES player(id)
 ); 
+
+CREATE VIEW wins AS
+SELECT t.id, COUNT(t.winner_id)
+FROM (SELECT p.id AS id, m.winner_id AS winner_id FROM player p LEFT JOIN match_result m ON p.id = m.winner_id) AS t 
+GROUP BY t.id; 
+
+CREATE VIEW losses AS
+SELECT t.id, COUNT(t.loser_id)
+FROM (SELECT p.id AS id, m.loser_id AS loser_id FROM player p LEFT JOIN match_result m ON p.id = m.loser_id) AS t
+GROUP BY t.id;
+
+CREATE VIEW player_standings AS
+SELECT t.id, t.name, t.wins, t.wins + losses.count AS matches
+FROM (SELECT player.id, player.name, wins.count AS wins
+FROM player JOIN wins ON player.id = wins.id) AS t
+JOIN losses ON t.id = losses.id
+ORDER BY t.wins DESC;
